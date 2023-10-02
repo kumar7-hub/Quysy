07f89f9a696bc853afa27fc42106a191d27e13a2
kumar7-hub <snehalkumar18@gmail.com>
2023-10-01 20:12:07 -0700

    updated

---
+++ src/components/Quiz.js
@@ -0,0 +1,38 @@
+import React, { useEffect } from 'react'
+import Questions from './Questions'
+
+/** redux store import */
+import { useSelector } from 'react-redux'
+
+export default function Quiz() {
+
+    const state = useSelector(state => state)
+
+    useEffect(() => {
+        console.log(state)
+    })
+
+    /** next button event handler */
+    function onNext() {
+        console.log('On next click');
+    }
+
+    /** Prev button event handler */
+    function onPrev() {
+        console.log('On onPrev click');
+    }
+
+    return (
+        <div className='container'>
+            <h1 className = 'title text-light'>Quiz Application</h1>
+
+            {/** display questions */}
+            <Questions />
+
+            <div className='grid'>
+                <button className='btn prev' onClick={onPrev}>Prev</button>
+                <button className='btn next' onClick={onNext}>Next</button>
+            </div>
+        </div>
+    )
+}
---
+++ src/database/data.js
@@ -0,0 +1,47 @@
+export default [
+    {
+        id: 1,
+        question : "Javascript is a _________ language.",
+        options : [
+            "Application",
+            "Scripting",
+            "Programming",
+        ],
+    },
+    {
+        id: 2,
+        question : "Following methods can be used to display data in some form using Javascript.",
+        options : [
+            "document.write()",
+            "console.log()",
+            "window.alert()",
+        ],
+    },
+    {
+        id: 3,
+        question : "When an operator is NULL, the typeof returned by the unary operator is _________.",
+        options : [
+            "boolean",
+            "number",
+            "string",
+        ],
+    },
+    {
+        id: 4,
+        question : "Which function is used to serialize an object into a JSON string?",
+        options : [
+            "stringify()",
+            "parse()",
+            "convert()",
+        ]
+    },
+    {
+        id : 5,
+        question: "What does the toString() method return?",
+        options : [
+            "return object",
+            "return string",
+            "return integer",
+        ]
+    }
+]
