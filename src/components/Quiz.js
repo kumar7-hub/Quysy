2681af704cb5cb97b4e4c281924663fa104325cd
kumar7-hub <snehalkumar18@gmail.com>
2023-10-02 08:20:58 -0700

    changed title

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -24,7 +24,7 @@ export default function Quiz() {
 
     return (
         <div className='container'>
-            <h1 className = 'title text-light'>Quiz Application</h1>
+            <h1 className = 'title text-light'>Quysy</h1>
 
             {/** display questions */}
             <Questions />
--- src/components/Result.js
+++ src/components/Result.js
@@ -12,7 +12,7 @@ export default function Result() {
 
     return (
         <div className = 'container'>
-            <h1 className = 'title text-light'>Quiz Application</h1>
+            <h1 className = 'title text-light'>Quysy</h1>
 
             <div className='result flex-center'>
                 <div className='flex'>
