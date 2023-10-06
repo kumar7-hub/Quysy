61c7ed3916dbc937b5211743396d9480f1acdcf5
kumar7-hub <snehalkumar18@gmail.com>
2023-10-05 17:05:44 -0700

    updated homepage screen

--- src/components/Main.js
+++ src/components/Main.js
@@ -13,8 +13,8 @@ export default function Main() {
             <ol>
                 <li>You will be asked 10 questions one after another.</li>
                 <li>10 points is awarded for the correct answer.</li>
-                <li>Each question has three options. You can choose only one options.</li>
-                <li>You can review and change answers before the quiz finish.</li>
+                <li>Each question has three options. You can choose only one option.</li>
+                <li>You can review and change answers before submitting quiz.</li>
                 <li>The result will be declared at the end of the quiz.</li>
             </ol>
 
